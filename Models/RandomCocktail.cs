5452f04bdf9e03457cf442690ba2a727cdf5c0da
Kovalchuk Nata <76482616+Kovalchuk-Nata@users.noreply.github.com>
2021-06-02 10:22:40 +0300

    Add files via upload

--- Models/RandomCocktail.cs
+++ Models/RandomCocktail.cs
@@ -1,53 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Telegram.Bot.Types.InputFiles;
 
 namespace jop.Models
 {
-    public class ExtendedIngredient    {
+    public class ExtendedIngredientsss
+    {
         public string Original { get; set; }
     }
 
-    public class Recipe    {
+    public class Recipes
+    {
         public string Title { get; set; }
         public int ReadyInMinutes { get; set; }
-        public List<ExtendedIngredient> ExtendedIngredients { get; set; }
+        public List<ExtendedIngredientsss> ExtendedIngredients { get; set; }
         public int Servings { get; set; }
         public string Image { get; set; }
         public string Instructions { get; set; }
-        public string GetRecipe()
+        public string GetRecipes()
         {
-            string rrr = $" {this.Title}\n" + "\nReady in minutes: " + $"{this.ReadyInMinutes}\n" + "\nIngredients: \n" + $"{this.GetIngredient()}" + "\nInstructions:\n" + $"{this.Instructions}";
+            string rrr = $" {this.Title}\n" + "\nReady in minutes: " + $"{this.ReadyInMinutes}\n" + "\nIngredients:\n" + $"{this.GetIngredient()}" + "\nInstructions:\n" + $"{this.Instructions}";
             string pattern = @"<ol>";
             string target = " ";
             Regex regex = new Regex(pattern);
             string result = regex.Replace(rrr, target);
             return rrr;
         }
-        public string GetPhoto()
+        public string GetPhotos()
         {
             string p = $"{this.Image}";
-            
+
             return p;
         }
 
         public string GetIngredient()
         {
             string text = " ";
-           
-            foreach(var i in ExtendedIngredients)
+
+            foreach (var i in ExtendedIngredients)
             {
-                text += i.Original+"\n";
+                text += i.Original + "\n";
             }
             return text;
         }
     }
 
-    public class RandomRecipe
+    public class RandomCocktail
     {
-        public Recipe[] Recipes { get; set; }
+        public Recipes[] Recipes { get; set; }
     }
+
 }
---
+++ Models/RecipeByName.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace jop.Models
+{
+    public class ExtendedIngredients
+    {
+        public string Original { get; set; }
+        public string GetIngredients()
+        {
+            string rrr = $"{this.Original}";
+            return rrr;
+        }
+    }
+
+    public class Steps
+    {
+        public int Number { get; set; }
+        public string Step { get; set; }
+    }
+
+    public class AnalyzedInstruction
+    {   
+        public List<Steps> Steps { get; set; }
+        public string GetInstruction()
+        {
+            string text = " ";
+
+            foreach (var i in Steps)
+            {
+                    text += i.Number + ") " + i.Step + "\n";
+            }
+            return text;
+        }
+    }
+
+    public class Result
+    {
+        public string Title { get; set; }
+        public List<ExtendedIngredients> ExtendedIngredients { get; set; }
+        public int ReadyInMinutes { get; set; }
+        public int Servings { get; set; }
+        public string Image { get; set; }
+        public List<AnalyzedInstruction> AnalyzedInstructions { get; set; }
+       
+        public string GetRecipe()
+        {
+            string rrr = $"{this.Title}\n" + "\n<b>Ready in minutes: </b>" + $"{this.ReadyInMinutes}\n" + "\n<b>Ingredients: </b>" + $"{this.GetIngredient()}\n" + "\n<b>Instructions:</b>\n" + $"<i>{this.GetInstruction()}</i>";
+            string pattern = @"<ol>";
+            string target = " ";
+            Regex regex = new Regex(pattern);
+            string result = regex.Replace(rrr, target);
+            return rrr;
+        }
+
+        public string GetIngredient()
+        {
+            string text = " ";
+
+            foreach (var i in ExtendedIngredients)
+            {
+                text += i.Original + "\n";
+            }
+            return text;
+        }
+
+        public string GetInstruction()
+        {
+            string text = " ";
+
+            foreach (var i in AnalyzedInstructions)
+            {
+                text += i.GetInstruction();
+            }
+            return text;
+        }
+        public string GetPhoto()
+        {
+            string p = $"{this.Image}";
+
+            return p;
+        }
+    }
+
+    public class RecipeByName
+    {
+        public Result[] Results { get; set; }
+    }
+}
